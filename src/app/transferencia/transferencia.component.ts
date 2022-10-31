81320ef55f677c4a3928a738cf3cfff190c37336
Aulas angular <pedromogly@gmail.com>
2022-10-31 17:10:54 -0300

    criando construtor Output

--- src/app/transferencia/transferencia.component.ts
+++ src/app/transferencia/transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-transferencia',
@@ -10,12 +10,21 @@ import { Component } from '@angular/core';
 })
 export class TransferenciaComponent {
 
+  @Output() aoEnviar = new EventEmitter<any>();
+
   valor: number = 11111;
   conta: number = 111;
 
   enviar() {
     console.log('Transferiu Macaco');
-    console.log('Valor enviado: ', this.valor);
-    console.log('Conta bancaria: ', this.conta);
+    //console.log('Valor enviado: ', this.valor);
+    //console.log('Conta bancaria: ', this.conta);
+    
+    //passar valor no Output aoEnviar diretamente
+    //this.aoEnviar.emit({valor: this.valor, conta: this.conta});
+
+    //passar valor no Output aoEnviar usando uma variavel valorEmitir
+    const valorEmitir = { valor: this.valor, conta: this.conta };
+    this.aoEnviar.emit(valorEmitir);
   }
 }
