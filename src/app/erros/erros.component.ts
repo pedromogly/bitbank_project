691a96f028cac6c112b317aaf8e4a663eab55138
Aulas angular <pedromogly@gmail.com>
2022-11-14 09:20:57 -0300

    começando estilização dos componentes de erro

--- src/app/erros/erros.component.ts
+++ src/app/erros/erros.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit , Input } from '@angular/core';
 @Component({
   selector: 'app-erros',
   templateUrl: './erros.component.html',
-  styleUrls: ['./erros.component.css']
+  styleUrls: ['./erros.component.css','./erros.component.scss']
 })
 export class ErrosComponent implements OnInit {
   @Input() erroRequisitado: any = {};
